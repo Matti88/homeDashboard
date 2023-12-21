b1a920435d7b0fae2c51063d488911ede85c25f0
S4W4A <matteo.montanari@erstegroup.com>
2023-12-21 22:30:17 +0100

    new tram Schedule

--- tramSchedule.js
+++ tramSchedule.js
@@ -1,22 +1,24 @@
+let latestData = null; // Variable to store the latest fetched data
+
+
 // Fetches departure data from the API
 async function fetchDepartureData() {
-    fetch("https://www.wienerlinien.at/ogd_realtime/monitor?activateTrafficInfo=stoerunglang&rbl=3439", {
-        headers: {
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-          // Include other necessary headers here
+ 
+
+    try {
+        const response = await fetch("https://thingproxy.freeboard.io/fetch/https://www.wienerlinien.at/ogd_realtime/monitor?activateTrafficInfo=stoerunglang&rbl=3439");
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-      
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
+        latestData = await response.json(); // Update the stored data
+    } catch (error) {
+        console.error('Failed to fetch departure data:', error);
     }
-    return response.json();
 }
 
-// Extracts the next five departure times from the data
+
+/// Extracts the next five departure times from the data and formats them
 function getNextFiveDepartures(data) {
     if (!data || !data.data || !data.data.monitors) {
         return [];
@@ -24,24 +26,34 @@ function getNextFiveDepartures(data) {
 
     const departures = data.data.monitors.flatMap(monitor => 
         monitor.lines.flatMap(line => 
-            line.departures.departure.map(d => d.departureTime.timeReal)
+            line.departures.departure.map(d => {
+                const dateTime = new Date(d.departureTime.timeReal);
+                return dateTime.getHours().toString().padStart(2, '0') + ':' + dateTime.getMinutes().toString().padStart(2, '0');
+            })
         )
     );
 
     return departures.slice(0, 5);
 }
 
+ 
+ 
+ 
 // Update the displayed times
 function updateDisplay() {
-    fetchDepartureData().then(data => {
-        const nextDepartures = getNextFiveDepartures(data);
+    if (latestData) {
+        const nextDepartures = getNextFiveDepartures(latestData);
         document.getElementById('nextTramTime').textContent = `Next departures: ${nextDepartures.join(', ')}`;
-    }).catch(error => {
-        console.error('Failed to fetch departure data:', error);
-        document.getElementById('nextTramTime').textContent = 'Failed to load departure times';
-    });
+    } else {
+        document.getElementById('nextTramTime').textContent = 'Waiting for data...';
+    }
 }
 
-// Initialize the update process
-setInterval(updateDisplay, 600000);
-updateDisplay();
+
+// Fetch data every hour
+setInterval(fetchDepartureData, 3600000); // 3600000 milliseconds = 1 hour
+fetchDepartureData(); // Initial fetch
+
+// Update display every 2 minutes
+setInterval(updateDisplay, 1200); // 120000 milliseconds = 2 minutes
+updateDisplay(); // Initial display update
